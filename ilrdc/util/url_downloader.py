ccb2f2bec634a6e2772f00ba1fad070c7fd0ceda
Retr0327 <48786838+Retr0327@users.noreply.github.com>
2021-10-29 14:59:43 +0800

    Added url_downloader.py

---
+++ ilrdc/util/url_downloader.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+
+def download_url(url: str) -> BeautifulSoup:
+    """The download_url function downloads the url.
+
+    Args:
+        url (str): the url
+
+    Returns:
+        a BeautifulSoup object
+    """
+    req = requests.get(url, headers={"user-agent": UserAgent.google()})
+    soup = BeautifulSoup(req.text, "lxml")
+    return soup
